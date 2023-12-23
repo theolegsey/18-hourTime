204612aa0b3dacadac1414456b6c2eaccc3e9e57
theolegsey <113287793+theolegsey@users.noreply.github.com>
2023-12-23 16:16:12 +0200

    Update sketch_nov11a.ino
    Bugs were fixed. Function addZoros was connected

--- sketch_nov11a.ino
+++ sketch_nov11a.ino
@@ -6,10 +6,11 @@ const int gpsTxPin = 3; // GPS RX to Arduino TX
 const int gpsPpsPin = 4; // Pulse Per Second pin
 const int gpsEnPin = 5; // Enable pin
 const int ledPin = 6;   // LED pin to indicate GPS fix status
-double h1, m1;          //transformed time to 18-hour format
 TinyGPSPlus gps;
 SoftwareSerial ssGPS(gpsRxPin, gpsTxPin);
-
+long tH(int hours, int minutes, int seconds);
+long tM(int hours, int minutes, int seconds);
+int Min;
 void setup() {
   Serial.begin(9600);
   ssGPS.begin(9600);
@@ -38,13 +39,38 @@ void loop() {
     digitalWrite(ledPin, LOW); // Turn off the LED if no GPS fix
   }
 }
-void transfotm18(int hours, int minutes, int seconds, float hour18, float minute18){
+/*void transfotm18(int hours, int minutes, int seconds, float hour18, float minute18){
   int commonT;
   hours*=3600;          //hours to seconds
   minutes*=60;          //minutes to seconds
   commonT=(hours+minutes+seconds)*0,135; //multiply 5,4*5,4*1,5 & divide on 324
   hour18=commonT/324;                    //full part hours
   minute18=(commonT % 324)*324;          //reminder*324 -minutes
+  }*/
+long tH(int hours, int minutes, int seconds){ 
+  long H;
+  float fV = 43.74;
+  double result;
+  H = hours*3600+65536;
+  H = H+minutes*60;
+  H =H+seconds;
+  //H = H *0.135;
+  
+  result = H*fV;
+  return result/104976;
+  }
+long tM(int hours, int minutes, int seconds){
+  long H2;
+  int H1;
+  float fV = 43.74;
+  double result;
+  H2 = hours*3600+65536;
+  H2 = H2+minutes*60;
+  H2 =H2+seconds;
+  result = H2*fV;
+  result = result/324;
+  H1 =int(result)%324;
+  return H1;
   }
 void displayGPSInfo() {
   // Display time, latitude, longitude, and altitude
@@ -54,12 +80,13 @@ void displayGPSInfo() {
     Serial.print(":");
     printDigits(gps.time.minute());
     Serial.print(":");
-    printDigits(gps.time.second()); 
+    printDigits(gps.time.second());
     Serial.println();
-    transfotm18(gps.time.hour(), gps.time.minute(),gps.time.second(), h1, m1);
-    Serial.print(h1);
+    Serial.print(tH(adjustedHour, gps.time.minute(), gps.time.second()));
     Serial.print(":");
-    Serial.println(m1);
+    Min = tM(adjustedHour, gps.time.minute(), gps.time.second());
+    addZeros(Min);
+    Serial.println();
   }
   
   Serial.print("Latitude: ");
@@ -80,15 +107,13 @@ void displayGPSInfo() {
   Serial.println();
 }
 void addZeros(int minutes){
-  String updatedZeros;
-  String stringMin = String(minutes, DEC);
  if(minutes<10){
-  updatedZeros = "00"+ stringMin;
+  Serial.print("00");
   }
  if(minutes<100&minutes>10){
-  updatedZeros = "0"+ stringMin;
+  Serial.print("0");
   }
-  return updatedZeros;
+  Serial.print(minutes);
   }
 void printDigits(int digits) {
   if (digits < 10) {
